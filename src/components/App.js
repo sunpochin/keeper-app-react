6690b869f4f3692c28c01ec96dd89413de66aa95
sun pochin <sunpochin@gmail.com>
2021-05-31 22:53:32 +0800

    complete challenge.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
+//jshint esversion:6
+
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
+import notes from "../notes";
+
 function App() {
+  function createNote(item) {
+    return (
+      <Note
+      key = {item.id}
+      title = {item.title}
+      content = {item.content}
+      />
+    );
+  }
+
   return (
     <div>
-      <Header />
-      <Note />
-      <Footer />
+    <Header / >
+    {notes.map(createNote)}
+    <Footer / >
     </div>
   );
 }
